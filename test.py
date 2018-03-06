8f7f730280c7985ea53589bd7034f7bf0d0ab61e
Pierce William O'Hara-Wild <pwoha1@student.monash.edu>
2018-03-07 10:41:36 +1100

    Sacrificed a bit of space complexity for a drastic time speed up.

--- test.py
+++ test.py
@@ -3,61 +3,47 @@ import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.pyplot as plt
 import colorsys
 from PIL import Image
-
-# (1) Import the file to be analyzed!
-img_file = Image.open("colour test.png")
-img = img_file.load()
-
-# (2) Get image width & height in pixels
-[xs, ys] = img_file.size
-max_intensity = 100
-hues = {}
 rgb_list = []
 rgb_freq = []
-# (3) Examine each pixel in the image file
-for x in range(0, xs):
-  for y in range(0, ys):
-    # (4)  Get the RGB color of the pixel
-    [r, g, b] = img[x, y]
-    found = False
-    for k in range(len(rgb_list)):
-        if rgb_list[k] == [r,g,b]:
-            rgb_freq[k] += 1
-            found = True
-            print("found duplicate")
-    if found == False:
-        rgb_list.append([r,g,b])
-        rgb_freq.append(1)
+colour_list = ["colour test.png","colour test2.png","colour test3.png","colour test4.png"]
+lookup_list = []
+for k in range(256):
+    lookup_list.append([])
+print(lookup_list[255])
 
-    # (5)  Normalize pixel color values
-    r /= 255.0
-    g /= 255.0
-    b /= 255.0
-for k in range(len(rgb_list)):
-    print(rgb_list[k],rgb_freq[k])
+for k in range(len(colour_list)):
+    # (1) Import the file to be analyzed!
+    img_file = Image.open(colour_list[k])
+    img = img_file.load()
+
+    # (2) Get image width & height in pixels
+    [xs, ys] = img_file.size
+    max_intensity = 100
+    hues = {}
+
+    # (3) Examine each pixel in the image file
+    for x in range(0, xs):
+      for y in range(0, ys):
+        # (4)  Get the RGB color of the pixel
+        [r, g, b] = img[x, y]
+
+        found = False
+
+        for k in range(len(lookup_list[r])):
+            if lookup_list[r][k] == [r,g,b]:
+                found = True
+                print("found duplicate")
+        if found == False:
+            print("appending")
+            lookup_list[r].append([r,g,b])
+            print(lookup_list[r])
 
 
+        # (5)  Normalize pixel color values
+        r /= 255.0
+        g /= 255.0
+        b /= 255.0
 
-img_file = Image.open("colour test.png")
-img = img_file.load()
-
-for x in range(0, xs):
-  for y in range(0, ys):
-    # (4)  Get the RGB color of the pixel
-    [r, g, b] = img[x, y]
-    found = False
-    for k in range(len(rgb_list)):
-        if rgb_list[k] == [r,g,b]:
-            rgb_freq[k] += 1
-            found = True
-            print("found duplicate")
-    if found == False:
-        rgb_list.append([r,g,b])
-        rgb_freq.append(1)
-
-    # (5)  Normalize pixel color values
-    r /= 255.0
-    g /= 255.0
-    b /= 255.0
-for k in range(len(rgb_list)):
-    print(rgb_list[k],rgb_freq[k])
+print("-----------------------INITILISATION COMPLETE-------------------------")
+print(lookup_list[146])
+print(lookup_list[1],lookup_list[85])
