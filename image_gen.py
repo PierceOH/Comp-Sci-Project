aa7b824ad11b0e20914f656335735c8e42a46eea
Pierce William O'Hara-Wild <pwoha1@student.monash.edu>
2018-03-25 12:18:20 +1100

    week 5 proggy
    built design of the front end GUI

--- image_gen.py
+++ image_gen.py
@@ -1,8 +1,13 @@
 #API KEY  #AIzaSyAnxHAQMnR-fqZAIgAyypVwPT1-r0u7aI8
 import requests
-def gen_map(long,lat,zoom):
+def gen_map(lat,long,zoom):
     g = open('static1.png','wb')
     g.write(requests.get(
-        'https://maps.googleapis.com/maps/api/staticmap?center=' + str(long)+',' + str(lat)+ ' &zoom=' + str(zoom)+'&size=530x640&scale=1&style=visibility:off&style=feature:road|element:geometry|color:99ff30|visibility:on&sensor=false').content)
+        'https://maps.googleapis.com/maps/api/staticmap?center=' + str(lat)+',' + str(long)+ ' &zoom=' + str(zoom)+'&size=530x640&scale=1&style=visibility:off&style=feature:road|element:geometry|color:99ff30|visibility:on&sensor=false').content)
     g.close()
-gen_map(-37.784002,145.219566,16)
+    f = open('static.png','wb')
+    f.write(requests.get(
+        'https://maps.googleapis.com/maps/api/staticmap?center='+ str(lat)+',' + str(long)+ ' &zoom=' + str(zoom)+ '&size=530x640').content)
+    f.close()
+gen_map(-37.93766034904226,145.22606529319148,16)
+print("SCAN COMPLETE")
