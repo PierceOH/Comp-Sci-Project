3800378bfba53b244036b846d4b67059c6a3de9c
Pierce William O'Hara-Wild <pwoha1@student.monash.edu>
2018-04-22 15:20:19 +1000

    added more GUI features
    a

--- webapp/app.js
+++ webapp/app.js
@@ -4,7 +4,10 @@ var router = express.Router();
 var path = __dirname + '/views/';
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-const Pageres = require('pageres');
+
+
+
+
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -19,11 +22,19 @@ router.get("/map",function(req,res){
   res.sendFile(path + "map.html");
 });
 
+router.get("/confirm",function(req,res){
+  res.sendFile(path + "confirm.html");
+});
+
 router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
+
 router.get("/load",function(req,res){
   res.sendFile(path + "load.html");
+});
+router.get("/results",function(req,res){
+  res.sendFile(path + "results.html");
 });
 
 app.use("/",router);
@@ -41,34 +52,36 @@ console.log("listening to port 3000:");
 io.listen(server).on('connection', function (socket) {
       console.log("Connected");
        socket.on('message', function (msg) {
-           console.log('Message Received: ', msg);
+          if (msg == "Area") {
+              console.log("Calculate area")
+              socket.emit('message', msg);
+              console.log('sent "COMPLETE"')
 
-           data_array = msg
-           lat = data_array[0].toString();
-           long = data_array[1].toString();
-           var fs = require('fs'),
-           request = require('request');
+              //https://www.npmjs.com/package/opencv  use for image detection! (RGB)
 
-           var download = function(uri, filename, callback){
-                request.head(uri, function(err, res, body){
-                      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-                      });
-                };
-        download('https://maps.googleapis.com/maps/api/staticmap?center='+ lat + ',' + long+ '&zoom=16&size=530x640', 'static1.png', function(){
-        console.log('done1');
-});
-//'https://maps.googleapis.com/maps/api/staticmap?center=' + str(lat)+',' + str(long)+ ' &zoom=' + str(zoom)+'&size=530x640&scale=1&style=visibility:off&style=feature:road|element:geometry|color:99ff30|visibility:on&sensor=false'
-download('https://maps.googleapis.com/maps/api/staticmap?center='+ lat + ',' + long+ '&zoom=16&size=530x640&scale=1&style=visibility:off&style=feature:road|element:geometry|color:99ff30|visibility:on&sensor=false', 'static2.png', function(){
-console.log('done2');
-});
+          } else {
+            data_array = msg
+            lat = data_array[0].toString();
+            long = data_array[1].toString();
+            console.log(lat,long);
+            var fs = require('fs'),
+            request = require('request');
 
-const delay = require('delay');
-delay(2000) //ensures synchronicity
-    .then(() => {
-      //socket.send('1')
-      socket.broadcast.emit('message', '1');
-      console.log('sent data');
-    });
+            var download = function(uri, filename, callback){
+                 request.head(uri, function(err, res, body){
+                       request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                       });
+                 };
+         download('https://maps.googleapis.com/maps/api/staticmap?center='+ lat + ',' + long+ '&zoom=16&size=600x640', 'static1.png', function(){
+         console.log('done1');
+ });
+ //'https://maps.googleapis.com/maps/api/staticmap?center=' + str(lat)+',' + str(long)+ ' &zoom=' + str(zoom)+'&size=530x640&scale=1&style=visibility:off&style=feature:road|element:geometry|color:99ff30|visibility:on&sensor=false'
+ download('https://maps.googleapis.com/maps/api/staticmap?center='+ lat + ',' + long+ '&zoom=16&size=530x640&scale=1&style=visibility:off&style=feature:road|element:geometry|color:99ff30|visibility:on&sensor=false', 'static2.png', function(){
+ console.log('done2');
+ });
+
+          }
+
 
  });
 
